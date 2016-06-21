dd9b1934ce97938d47287e30a36d30ac4eece7d6
Simon MacDonald <simon.macdonald@gmail.com>
2016-06-21 10:23:56 -0400

    Switch to clobber to call registration event

--- src/browser/ServiceWorker.js
+++ src/browser/ServiceWorker.js
@@ -1,12 +1,12 @@
-self.addEventListener("install", function(event) {
+self.addEventListener('install', function(event) {
     self.skipWaiting();
-    console.log("SW Installed", event);
+    console.log('SW Installed', event);
 });
 
-self.addEventListener("activate", function(event) {
+self.addEventListener('activate', function(event) {
     console.log('*** Activated', event);
 });
 
-self.addEventListener("push", function(event) {
-    console.log("Push message received", event);
+self.addEventListener('push', function(event) {
+    console.log('Push message received', event);
 });
